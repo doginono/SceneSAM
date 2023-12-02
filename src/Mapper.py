f25765870c51ac65d4578cf93707b6d77b15b254
Julius Koerner <ge35beh@mytum.de>
2023-12-02 16:31:04 +0100

    changed bottom

--- src/Mapper.py
+++ src/Mapper.py
@@ -560,10 +560,10 @@ class Mapper(object):
             loss = torch.abs( #J: we backpropagate only through depth in stage middle and fine
                 batch_gt_depth[depth_mask]-depth[depth_mask]).sum()
             if writer is not None:
-                writer.add_scalar(f'Loss/depth', loss.item()/torch.sum(depth_mask), idx)
+                writer.add_scalar(f'Loss/depth', loss.item()/torch.sum(depth_mask), idx*(num_joint_iters+inc)+joint_iter)
             if (self.stage == 'color'): #J: changed it from condition not self.nice or self.stage == 'color'
                 color_loss = torch.abs(batch_gt_color - color_semantics).sum()
-                writer.add_scalar(f'Loss/color', color_loss.item()/color_semantics.shape[0], idx)
+                writer.add_scalar(f'Loss/color', color_loss.item()/color_semantics.shape[0], idx*(num_joint_iters+inc)+joint_iter)
                 weighted_color_loss = self.w_color_loss*color_loss
                 loss += weighted_color_loss
             #-----------------added-------------------
@@ -571,7 +571,7 @@ class Mapper(object):
                 loss_function = torch.nn.CrossEntropyLoss()
                 semantic_loss = loss_function(color_semantics, batch_gt_semantic)
                 print('SEMANTIC LOSS ', semantic_loss.item()/color_semantics.shape[0]) 
-                writer.add_scalar(f'Loss/semantic', semantic_loss.item()/color_semantics.shape[0], idx)
+                writer.add_scalar(f'Loss/semantic', semantic_loss.item()/color_semantics.shape[0], idx*(num_joint_iters+inc)+joint_iter)
                 weighted_semantic_loss = self.w_semantic_loss*semantic_loss
                 loss += weighted_semantic_loss
             #-----------------end-added-------------------
