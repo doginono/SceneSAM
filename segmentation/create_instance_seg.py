0718c24859e2d161e6311244539b0bdee3bf99c0
Julius Körner <julius.koerner@nebumind.com>
2023-12-13 17:47:51 +0100

    created instance encoding

--- segmentation/create_instance_seg.py
+++ segmentation/create_instance_seg.py
@@ -1,5 +1,8 @@
 import numpy as np
 import cv2
+import os
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
+
 
 
 def masks2encoding(masks):
@@ -33,3 +36,30 @@ def path2instances(path, mask_generator):
     masks = mask_generator.generate(image)
     instances = masks2encoding(masks)
     return instances
+
+def instance_encoding2file(encoding, path):
+    """_summary_
+
+    Args:
+        encoding (np.array): with shape (height, width) and values in [0, len(masks)-1]
+        path (str): path to save the encoding
+    """
+    np.save(path, encoding)
+    
+#TODO implement in nice-slam later
+def create_sam():
+    """_summary_
+
+    Returns:
+        SamAutomaticMaskGenerator: An SamAutomaticMaskGenerator object
+    """
+    sam_checkpoint = "/home/julius/models/sam_vit_h_4b8939.pth"
+    model_type = "vit_h"
+
+    device = "cuda"
+
+    sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+    sam.to(device=device)
+
+    mask_generator = SamAutomaticMaskGenerator(sam)
+    return mask_generator
