d5811230854af76118689431825f640453f7f209
dogu.tamgac <dogu.tamgac@t-systems.com>
2023-12-20 17:31:22 +0100

    Added mapping

--- segmentation/id_generation.py
+++ segmentation/id_generation.py
@@ -6,46 +6,45 @@ import torch
 import backproject
 
 
-
 def readDepth(filepath):
-    depth=cv2.imread(filepath, cv2.IMREAD_UNCHANGED)
+    depth = cv2.imread(filepath, cv2.IMREAD_UNCHANGED)
     depth_data = depth.astype(np.float32) / 6553.5
     depth_data = torch.from_numpy(depth_data)
     return depth_data
 
-def get_ids(masks1, masks2, instance=10):
-    ids1 = generateIds(masks1)
-    ids2 = generateIds(masks2)
-    samplesFromCurrentMask = sample_from_instances(masks2, ids2, points_per_instance=5)
 
-    backprojectedSamples, _ = backproject.backproject(
-        samplesFromCurrentMask[:, :, instance], Tf, Tg, K, Depthf
-    )
+# check one class mapping
+def createMapping(
+    ids1, ids2, backprojectedSamples, samplesFromCurrentMask, instance=10
+):
+    points_per_instance = 5
     backprojectedSamples = backprojectedSamples.astype(int)
-
-    backProjectedIds = []
-    for i, backProjsample in enumerate(
-        list(zip(backprojectedSamples[0], backprojectedSamples[1]))
-    ):
-        if (
-            backProjsample[1] < 0
-            or backProjsample[0] < 0
-            or backProjsample[1] > 679
-            or backProjsample[0] > 1199
-        ):
-            print("Outside of image bounds:", backProjsample)
-            backProjectedIds.append(-1)
-        else:
-            print("inside of image bounds:", backProjsample)
-            backProjectedIds.append(ids1[backProjsample[1]][backProjsample[0]])
-    return (
-        backprojectedSamples,
-        samplesFromCurrentMask[:, :, instance],
-        backProjectedIds,
+    # efficient
+    # filter out samples outside of image bounds
+    condition = (
+        (backprojectedSamples[1, :] < 0)
+        | (backprojectedSamples[0, :] < 0)
+        | (backprojectedSamples[1, :] > 679)
+        | (backprojectedSamples[0, :] > 1199)
     )
+    filteredBackProj = backprojectedSamples[:, ~condition]
 
-Depthg = readDepth(path_to_frames + "depth000000.png")
-Depthf = readDepth(path_to_frames + "depth000020.png")
-K = np.array([[600, 0.0, 599.5], [0.0, 600, 339.5], [0.0, 0.0, 1.0]])
-Tg = T_wc[0]
-Tf = T_wc[20]
+    numOutofBounds = points_per_instance - len(filteredBackProj[0])
+    print("numOutofBounds", numOutofBounds)
+    mapping = {}
+    elementsBackprojected = np.array(
+        list(zip(filteredBackProj[0], filteredBackProj[1]))
+    )
+    elementesCurrentFrame = np.array(
+        list(zip(samplesFromCurrentMask[0], samplesFromCurrentMask[1]))
+    )
+    print(elementesCurrentFrame.shape)
+    print(elementesCurrentFrame)
+    ids1_elements = ids1[elementsBackprojected[:, 1], elementsBackprojected[:, 0]]
+    ids2_elements = ids2[elementesCurrentFrame[:, 1], elementesCurrentFrame[:, 0]]
+    print("ids1_elements", ids1_elements)
+    print("ids2_elements", ids2_elements)
+
+    # output: instances number-> to one earlier frame
+    # np unique mapping with OutofBounds
+    return filteredBackProj, numOutofBounds
