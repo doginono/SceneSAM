4ef049d495a91a85b9a099ead81c8e98a3571dcd
JuliusKoerner <ge35beh@mytum.de>
2024-03-28 11:24:52 +0100

    run_timing update

--- run_timing.sh
+++ run_timing.sh
@@ -2,8 +2,8 @@
 
 # Define your commands in this array
 declare -A commands
-# commands=(["command1"]="python -W ignore run.py configs/Own/room1.yaml" ["command2"]="python -W ignore run.py configs/Own/room2.yaml" ["command3"]="python -W ignore run.py configs/Own/room0.yaml" ["command4"]="python -W ignore run.py configs/Own/office0.yaml" ["command5"]="python -W ignore run.py configs/Own/office1.yaml" ["command6"]="python -W ignore run.py configs/Own/office2.yaml" ["command7"]="python -W ignore run.py configs/Own/office3.yaml" ["command8"]="python -W ignore run.py configs/Own/office4.yaml" ["command9"]="python -W ignore run.py configs/Own/room0_small.yaml" ["command10"]="python -W ignore run.py configs/Own/room1_small.yaml")
-commands=(["command1"]="python -W ignore run.py configs/Own/room1_small.yaml")
+commands=(["command1"]="python -W ignore run.py configs/Own/room1.yaml" ["command2"]="python -W ignore run.py configs/Own/room2.yaml" ["command3"]="python -W ignore run.py configs/Own/room0.yaml" ["command4"]="python -W ignore run.py configs/Own/office0.yaml" ["command5"]="python -W ignore run.py configs/Own/office1.yaml" ["command6"]="python -W ignore run.py configs/Own/office2.yaml" ["command7"]="python -W ignore run.py configs/Own/office3.yaml" ["command8"]="python -W ignore run.py configs/Own/office4.yaml" ["command9"]="python -W ignore run.py configs/Own/room0_small.yaml" ["command10"]="python -W ignore run.py configs/Own/room1_small.yaml")
+#commands=(["command1"]="python -W ignore run.py configs/Own/room1_small.yaml")
 declare -A execution_times
 
 for cmd in "${!commands[@]}"; do
