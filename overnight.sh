106deacfff336dce0980f92a9e16ee02bff68de7
JuliusKoerner <ge35beh@mytum.de>
2024-04-27 19:55:48 +0200

    configs and overnight

--- overnight.sh
+++ overnight.sh
@@ -1,33 +1,48 @@
 #!/bin/bash
 
-# Navigate to the project directory
-cd /home/rozenberszki/D_Project/wsnsl/
-
 # Timeout duration in seconds (3 hours = 10800 seconds)
 timeout_duration=13500
 
-# Run each command with a timeout
+cd /home/rozenberszki/project/wsnsl/
 
-timeout $timeout_duration python -W ignore run.py configs/Scannet++/8b5caf3398.yaml
+timeout $timeout_duration python -W ignore run.py configs/ScanNet/scene0616_00.yaml
 sleep 100
 
-
-timeout $timeout_duration python -W ignore run.py configs/Scannet++/b20a261fdf.yaml
+timeout $timeout_duration python -W ignore run.py configs/ScanNet/scene0300_01.yaml
 sleep 100
 
-
-timeout $timeout_duration python -W ignore run.py configs/Scannet++/fe1733741f.yaml
+timeout $timeout_duration python -W ignore run.py configs/ScanNet/scene0427_00.yaml
 sleep 100
 
-timeout $timeout_duration python -W ignore run.py configs/Scannet++/41b00feddb.yaml
-sleep 100
-
-
-timeout $timeout_duration python -W ignore run.py configs/Scannet++/98b4ec142f.yaml
-sleep 100
+# Navigate to the project directory
+cd /home/rozenberszki/D_Project/wsnsl/
 
 timeout $timeout_duration python -W ignore run.py configs/Scannet++/56a0ec536c.yaml
 sleep 100
+
+timeout $timeout_duration python -W ignore run.py configs/Scannet++/c413b34238.yaml
+sleep 100
+
+#timeout $timeout_duration python -W ignore run.py configs/Scannet++/8b5caf3398.yaml
+#sleep 100
+
+
+#timeout $timeout_duration python -W ignore run.py configs/Scannet++/b20a261fdf.yaml
+#sleep 100
+
+
+#timeout $timeout_duration python -W ignore run.py configs/Scannet++/fe1733741f.yaml
+#sleep 100
+
+#timeout $timeout_duration python -W ignore run.py configs/Scannet++/41b00feddb.yaml
+#sleep 100
+
+
+#timeout $timeout_duration python -W ignore run.py configs/Scannet++/98b4ec142f.yaml
+#sleep 100
+
+#timeout $timeout_duration python -W ignore run.py configs/Scannet++/56a0ec536c.yaml
+#sleep 100
 
 
 timeout $timeout_duration python -W ignore run.py configs/Scannet++/5fb5d2dbf2.yaml
