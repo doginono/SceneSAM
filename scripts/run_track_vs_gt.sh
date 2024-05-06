04544a46777abf9123f1cc82f682f42e684e16a0
JuliusKoerner <ge35beh@mytum.de>
2024-05-06 13:43:04 +0200

    configs scripts

--- scripts/run_track_vs_gt.sh
+++ scripts/run_track_vs_gt.sh
@@ -9,12 +9,22 @@ current_dir=$(pwd)
 # Define the log file path using the current directory
 log_file="$current_dir/execution_times_track_vs_gt.txt"
 
-start_time=$(date +%s)
-python -W ignore run.py configs/Own/room0_gt_tracking.yaml
-end_time=$(date +%s)
-echo "Execution time for room 0 gt: $(($end_time - $start_time)) seconds" >> $log_file
+#start_time=$(date +%s)
+#python -W ignore run.py configs/Own/room0_gt_tracking.yaml
+#end_time=$(date +%s)
+#echo "Execution time for room 0 gt: $(($end_time - $start_time)) seconds" >> $log_file
+
+#start_time=$(date +%s)
+#python -W ignore run.py configs/Own/room0.yaml
+#end_time=$(date +%s)
+#echo "Execution time for room0 tracking activated: $(($end_time - $start_time)) seconds" >> $log_file
 
 start_time=$(date +%s)
-python -W ignore run.py configs/Own/room0.yaml
+python -W ignore run.py configs/Own/room0_panoptic_sms0.yaml
+end_time=$(date +%s)
+echo "Execution time for room0 tracking activated: $(($end_time - $start_time)) seconds" >> $log_file
+
+start_time=$(date +%s)
+python -W ignore run.py configs/Own/room0_panoptic_sms_controll.yaml
 end_time=$(date +%s)
 echo "Execution time for room0 tracking activated: $(($end_time - $start_time)) seconds" >> $log_file
