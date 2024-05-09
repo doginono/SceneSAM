aefa33209723e42f2747017d452f4e385146a2f6
doginono <dogu.tamgac@gmail.com>
2024-05-09 17:06:39 +0200

    merge

--- runDavid.sh
+++ runDavid.sh
@@ -13,6 +13,15 @@ current_dir=$(pwd)
 #end_time=$(date +%s)
 #echo "Execution time for 07f5b601ee WITHOUT GROUNDTRUTH: $(($end_time - $start_time)) seconds" #>> $log_file
 
+start_time=$(date +%s)
+python -W ignore run.py configs/Scannet++/7cd2ac43b4.yaml
+end_time=$(date +%s)
+
+
+start_time=$(date +%s)
+python -W ignore run.py configs/Scannet++NoTruth/7cd2ac43b4.yaml
+end_time=$(date +%s)
+
 
 start_time=$(date +%s)
 python -W ignore run.py configs/Scannet++/07f5b601ee.yaml
@@ -23,13 +32,3 @@ start_time=$(date +%s)
 python -W ignore run.py configs/Scannet++NoTruth/07f5b601ee.yaml
 end_time=$(date +%s)
 
-
-
-start_time=$(date +%s)
-python -W ignore run.py configs/Scannet++/7cd2ac43b4.yaml
-end_time=$(date +%s)
-
-
-start_time=$(date +%s)
-python -W ignore run.py configs/Scannet++NoTruth/7cd2ac43b4.yaml
-end_time=$(date +%s)
